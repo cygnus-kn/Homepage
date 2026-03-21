af968e6d8fbbd78c876df5cd559ed1485e454bd4
cygnus-kn <hiepbq14408@gmail.com>
2026-03-21 17:56:36 +0700

    Perform massive hardcode synchronization from completely custom user local storage payload

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
       icon: "📚",
       links: [
         { title: "Astral Codex Ten", url: "https://www.astralcodexten.com" },
-        { title: "ACOUP", url: "https://acoup.blog" },
         { title: "Escaping Flatland", url: "https://www.henrikkarlsson.xyz" },
         { title: "Numb at the Lodge", url: "https://samkriss.substack.com" },
+        { title: "ACOUP", url: "https://acoup.blog" },
         { title: "Maximum Progress", url: "https://www.maximum-progress.com" },
         { title: "Matt Lakeman", url: "https://mattlakeman.org/" },
         { title: "not not Talmud", url: "https://notnottalmud.substack.com", iconUrl: "https://substackcdn.com/image/fetch/$s_!nZQz!,w_80,h_80,c_fill,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F59a1514d-5160-43db-83e1-c1e5dc5de2ee_1280x1280.png" },
@@ -57,9 +57,58 @@ const CONFIG = {
       ]
     },
     {
-      name: "Productivity",
-      icon: "⚡",
-      links: []
+      name: "Video Essay",
+      icon: "🎥",
+      links: [
+        { title: "Solar Sands", url: "https://www.youtube.com/@SolarSands", iconUrl: "https://yt3.ggpht.com/6tmkMODnk0I0DbVBbJ6MtgFfLNuoxvIlIEID9OyArF0W0sDvfKNrnfKncaqXXmsPEKku8xV_bt8=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "EmpLemon", url: "https://www.youtube.com/@EmperorLemon", iconUrl: "https://yt3.ggpht.com/F-ULo6Ryi7IHofqMHzF7qEieFsfhIuRI8Tv7VVqinU2tjaob6LMtLVEVEAn0hpzqp7amUKyS=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "LEMMiNO", url: "https://www.youtube.com/@LEMMiNO", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_n9UULHnAEc-yMg6URdgtNDvUqeu-SXR3WMV2ObydPnLg=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Jacob Geller", url: "https://www.youtube.com/@JacobGeller", iconUrl: "https://yt3.ggpht.com/oEvJp21jyoK8viSJQZsoIB4TpDmXsRBjOfMybj8azcfgCQ6dkQ8uP6Fg4nyYUCosnYhYzTlc=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Nerdwriter1", url: "https://www.youtube.com/@Nerdwriter1", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_lrqB-g_-yHIwNQwBMXcAIEo7fOtKd1Fv3ZPnFv0D3pm6g=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Fredrik Knudsen", url: "https://www.youtube.com/@FredrikKnudsen", iconUrl: "https://yt3.ggpht.com/WqW4AFB01iqkWFImbyckVHPjWuEbld_fh27L8doMFDsqFzllfLuf8oQssTIOQlcimh8FzB6DvQ=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "The Exploring Series", url: "https://www.youtube.com/@TheExploringSeries", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_mmxYfwLW6fNAcclSxC0pE9NkcjyOl0iTSNsfsqYq0BHg=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Defunctland", url: "https://www.youtube.com/@Defunctland", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_nbqSLm8QyE1Wzo-wB5PqqXjkDqdZfcxbUQkwzbgX4uv1g=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Exurb1a", url: "https://www.youtube.com/@Exurb1a", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_nL_Jo7sq5NSk1zuJQg-_18AOpQL31eeyXP01Pv-ACciaQ=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Company Man", url: "https://www.youtube.com/@companyman114", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_k6haZxIjvv5TbqgDUHY13vLPckV8gGOTfAwvkUxk3JyeY=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Acolytes Of Horror", url: "https://www.youtube.com/@AcolytesOfHorror", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_ll3SJJy-Mk0EqKyDuQisKY3nrjgOXDtgNXWNdMpvl7ppI=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "hbomberguy", url: "https://www.youtube.com/@hbomberguy", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_mxzfZOQ4VUZ45Hu8oPlkl6xz9OVnHKpn4yKPkNcOVQGcI=s88-c-k-c0x00ffffff-no-rj" }
+      ]
+    },
+    {
+      name: "Education",
+      icon: "🎓",
+      links: [
+        { title: "JSC - Criminal Psychology", url: "https://www.youtube.com/@JCS", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_kctKVxcu-jhgLMpmgHsv-UFmQku6-a_eNVC9C_RCxlkPw=s88-c-k-c0x00ffffff-no-rj" }
+      ]
+    },
+    {
+      name: "Cinema & TV",
+      icon: "🍿",
+      links: [
+        { title: "Accented Cinema", url: "https://www.youtube.com/@AccentedCinema", iconUrl: "https://yt3.ggpht.com/agIi1DZMbZpoZQ-yweEHwGUFzAQW1tyWP8IF12s-jrqwnZHuJidA5FI21BWcbi0zZgO1ynHd=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Schaffrillas Productions", url: "https://www.youtube.com/@SchaffrillasProductions", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_nUUgl5CPnR7vXcf40M6y-bsNYBOUqMJkZyxm3eh78kvC0=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "The Sin Squad", url: "https://www.youtube.com/@The_Sin_Squad", iconUrl: "https://yt3.ggpht.com/-3lh7aVN1sp4FPTEOgRPPGHd1mtHFjeRKZm-ZI9kr5GKKgd2vhX9J-8N6c8z_16QJq9EEutu=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "schnee", url: "https://www.youtube.com/@schnee1", iconUrl: "https://yt3.ggpht.com/0nmWB8a-J3WSoFXgnr-5RZTKTy6DMwhhSAbPaeFPqimY8_S_hIuKgHHk7soqpHLSJro6DuSCiQ=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Sloan Stowe", url: "https://www.youtube.com/@SloanStowe", iconUrl: "https://yt3.ggpht.com/wReGfua0kSnIrt8snoqfvqiiwzxQxAKW4ljTPdasCnQzebyK701NwDiliXTs6wFUKv0K0tyfXw=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "captainmidnight", url: "https://www.youtube.com/@captainmidnight", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_k66x-646xTKINzEwR_HB165YmHTxfb0n6af6jVR1Y_yno=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "CZsWorld", url: "https://www.youtube.com/@CZsWorld", iconUrl: "https://yt3.ggpht.com/gbRZyXFSzzPkqY9Bj1y4x_QcpgkfRJsRvz-pCCayj2AW9Af_c9048DXbL5cDC7HgphMhF5GUKmQ=s88-c-k-c0x00ffffff-no-rj" }
+      ]
+    },
+    {
+      name: "Animation",
+      icon: "🎨",
+      links: [
+        { title: "TheOdd1sOut", url: "https://www.youtube.com/@theodd1sout", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_mxpQ2hfudhflswgM8pZg22Jn_bgaQV0gIpUqYQW4z9Av8=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "JaidenAnimations", url: "https://www.youtube.com/@jaidenanimations", iconUrl: "https://yt3.ggpht.com/6uDu4HmbcorfDWch6L4FAzv-DFMOstOwhTks-5VUm-kY5puZ_oU4EeA1YOqEM_EDvCTj3UPUW68=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "GingerPale", url: "https://www.youtube.com/@GingerPale", iconUrl: "https://yt3.ggpht.com/ll7jGEeEKOrG5UsfA6jD8VvpbZwpYK8ciS1JRYG0pO6Ay09rjz4THGVN78JFoS5SlSdgYtYlgPo=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "BlueJay", url: "https://www.youtube.com/@BlueJayYT", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_mtE0wtRYXirpEWGKtJ_mK85JBizT2WktAw6QBpDsz-OA=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Ice Cream Sandwich", url: "https://www.youtube.com/@IceCreamSandwich", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_nfaehZCNR35m9QPE3-s4pB9Aww7U3mWG_zlYeeymlu3T4=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Casually Explained", url: "https://www.youtube.com/@CasuallyExplained", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_lAO_lHZfWaYLuWbSBxQzUkPLQ-2xRvPdSC8KpvfUWzfX4=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "the poopie show", url: "https://www.youtube.com/@thepoopieshow", iconUrl: "https://yt3.ggpht.com/sPRCC8y6VzobcPaFkkyPmItcVUqgJBJJ3LcIqADi-Jv7Sxv9V4yU-cAjfcbUgLwE-OV-8nhTsA=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Sam O'Nella Academy", url: "https://www.youtube.com/@SamONellaAcademy", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_l6Uep21u0mI0cPOY5h2YMxXyRCajmvy62-TlrOaF0r-pQ=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Misinformed", url: "https://www.youtube.com/@Misinformed_yt", iconUrl: "https://yt3.ggpht.com/ytc/AIdro_ll-N0GASu67EIoGqNvLsB0FGHAeYpgXT1XfWrLnx5ifA=s88-c-k-c0x00ffffff-no-rj" },
+        { title: "Mostly True", url: "https://www.youtube.com/@MostlyTrue", iconUrl: "https://yt3.ggpht.com/2a95phYISx1m5lmFjKYzOcSYc-53YpSX7lwxLWu8r0WeB4yZ41HyERdmybbJF9B23K6-FAWq8Q=s88-c-k-c0x00ffffff-no-rj" }
+      ]
     }
   ]
 };
